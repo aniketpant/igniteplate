f2e4cc3d6a629a29c3890cb7f708ad7178b6295e
aniketpant <me@aniketpant.com>
2012-06-05 11:36:59 +0530

    updated config files

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -1,45 +1,66 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 $config = array(
-            'login' => 
+            'standard/login' => 
+            array(
                 array(
-                    array(
-                        'field'   => 'email',
-                        'label'   => 'Email',
-                        'rules'   => 'xss_clean|required|valid_email'
-                      ),
-                    array(
-                        'field'   => 'password',
-                        'label'   => 'Password',
-                        'rules'   => 'xss_clean|required'
-                      )
+                    'field'   => 'user_name',
+                    'label'   => 'Username',
+                    'rules'   => 'xss_clean|required'
+                  ),
+                array(
+                    'field'   => 'password',
+                    'label'   => 'Password',
+                    'rules'   => 'xss_clean|required'
+                  )
             ),
-            'signup' => 
+            'standard/signup' => 
+            array(
                 array(
-                    array(
-                        'field'   => 'email',
-                        'label'   => 'Email',
-                        'rules'   => 'xss_clean|required|valid_email'
-                      ),
-                    array(
-                        'field'   => 'password',
-                        'label'   => 'Password',
-                        'rules'   => 'xss_clean|required|matches[passconf]|min_length[6]'
-                      ),
-                    array(
-                        'field'   => 'passconf',
-                        'label'   => 'Confirm Password',
-                        'rules'   => 'xss_clean|required'
-                    )
-            ),
-            'forgot_password'   =>
+                    'field'   => 'user_name',
+                    'label'   => 'Username',
+                    'rules'   => 'xss_clean|required'
+                  ),
                 array(
-                    array(
-                        'field' => 'email',
-                        'label' => 'Email',
-                        'rules' => 'xss_clean|is_email|required'
-                    )
+                    'field'   => 'password',
+                    'label'   => 'Password',
+                    'rules'   => 'xss_clean|required|matches[passconf]|min_length[6]'
+                  ),
+                array(
+                    'field'   => 'passconf',
+                    'label'   => 'Confirm Password',
+                    'rules'   => 'xss_clean|required'
                 )
+            ),
+            'standard/forgot_password' =>
+            array(
+                array(
+                    'field' => 'user_name',
+                    'label' => 'Username',
+                    'rules' => 'xss_clean|is_email|required'
+                )
+            ),
+            'standard/change_password' =>
+            array(
+                array(
+                    'field'   => 'password',
+                    'label'   => 'Password',
+                    'rules'   => 'xss_clean|required|matches[passconf]|min_length[6]'
+                  ),
+                array(
+                    'field'   => 'passconf',
+                    'label'   => 'Confirm Password',
+                    'rules'   => 'xss_clean|required'
+                )
+            ),
+            'admin/settings' =>
+            array(
+                array(
+                    'field' => 'site_name',
+                    'label' => 'Site Name',
+                    'rules' => 'xss_clean|required'
+                )
+            ),
         );
 
 /* End of file form_validation.php */
