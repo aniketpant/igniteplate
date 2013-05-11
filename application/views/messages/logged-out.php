d76116d826c74f01ab27e1d5a8a842b89f98ab5a
Aniket Pant <me@aniketpant.com>
2013-05-11 23:13:22 +0530

    Start afresh Using Codeigniter v2.1.0 Project will be built with Travis CI
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- application/views/messages/logged-out.php
+++
@@ -1,8 +0,0 @@
-<?php include 'application/views/inc/header.php'; ?>
-
-                <section>
-                    <h1>You have been logged out.</h1>
-                    <p>To login again, click <a class="btn btn-primary" href="<?php echo site_url(); ?>/main/login"> here</a></p>
-                </section>
-
-<?php include 'application/views/inc/footer.php'; ?>
--- application/views/user/profile.php
+++
@@ -1,8 +0,0 @@
-<?php include 'application/views/inc/header.php'; ?>
-
-                <section>
-                    <h1>Profile</h1>
-                    <p>This is how your profile looks.</p>
-                </section>
-
-<?php include 'application/views/inc/footer.php'; ?>
